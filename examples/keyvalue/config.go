ef7274ba331ad5dcab38b2c93dbc341f0bd46dec
Shawn Burke <sburke@uber.com>
2016-08-09 12:05:53 -0700

    Add config to examples

---
+++ examples/keyvalue/config.go
@@ -0,0 +1,5 @@
+package main
+
+type serviceConfig struct {
+	SomeNumber int `yaml:"some_number"`
+}
--- examples/keyvalue/service_app.go
+++ examples/keyvalue/service_app.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/uber-go/uberfx/core"
 )
 
 type ServiceApp struct {
 	core.Service
+	Config serviceConfig
 }
 
 var _ core.ServiceInstance = &ServiceApp{}
 
 func (service *ServiceApp) OnInit(svc *core.Service) error {
+	fmt.Printf("The config value is %v\n", service.Config.SomeNumber)
 	return nil
 }
 
