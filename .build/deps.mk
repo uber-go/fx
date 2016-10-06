23b4d6fc4136b9181268d158153dc0a0169b631f
Glib Smaga <glibsm@users.noreply.github.com>
2016-10-06 16:34:50 -0700

    Remove obsolete SHUOLD_LINT flag (#69)

--- .build/deps.mk
+++ .build/deps.mk
@@ -7,12 +7,8 @@ dependencies:
 	$(ECHO_V)go install ./vendor/github.com/axw/gocov/gocov
 	$(ECHO_V)go install ./vendor/github.com/mattn/goveralls
 	$(ECHO_V)go install ./vendor/github.com/go-playground/overalls
-ifdef SHOULD_LINT
 	@echo "Installing golint..."
 	$(ECHO_V)go install ./vendor/github.com/golang/lint/golint
-else
-	@echo "Not installing golint, since we don't expect to lint on" $(GO_VERSION)
-endif
 
 GOCOV := gocov
 OVERALLS := overalls
