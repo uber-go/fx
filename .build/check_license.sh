8b8174d61e3c6b417db14cd75932b369a22454f8
Aiden Scandella <sectioneight@users.noreply.github.com>
2016-10-09 13:16:24 -0700

    Make lint smart and auto-add headers (#82)
    * Make lint smart and auto-add headers
    
    * Skip npm in CI
    
    * Simplify

--- .build/check_license.sh
+++ .build/check_license.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+set -eu
+set -o pipefail
+
+# Try to use npm/uber-licence if node is available
+LICENCE_BIN="uber-licence"
+run_uber_licence() {
+  local bin="$LICENCE_BIN"
+  # Ok, somebody hasn't run `npm install -g uber-licence`, that's cool
+  if ! which "$bin" >/dev/null ; then
+    npm install "$bin"
+    bin="./node_modules/uber-licence/bin/licence"
+  fi
+
+  readonly local output=$("$bin" --file "*.go" | sed "s/^fix //")
+  if [ -z "$output" ]; then
+    exit 0
+  fi
+
+  echo "The following files were missing licence headers."
+  echo "Please amend your commit."
+  echo
+  echo "$output"
+  exit 1
+}
+
+set +u
+# Don't even try in CI, node is flaky
+if [ -z "$CI" ] ; then
+  if which uber-licence >/dev/null || which npm >/dev/null; then
+    run_uber_licence
+  fi
+fi
+set -u
 
 text=$(head -1 LICENSE.txt)
 
--- .build/licence.mk
+++ .build/licence.mk
@@ -1,7 +1,6 @@
-LICENCE_FORMAT := "*.go"
-
-LICENCE_BIN := uber-licence
+_THIS_LICENCE_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+_THIS_LICENCE_DIR := $(dir $(_THIS_LICENCE_MAKEFILE))
 
 add-uber-licence:
-	which $(LICENCE_BIN) > /dev/null || npm install -g $(LICENCE_BIN)
-	$(LICENCE_BIN) --file $(LICENCE_FORMAT)
+	$(ECHO_V)OUTPUT=$($(_THIS_LICENCE_DIR)/check_licence.sh) ; \
+		[ -z "$$(OUTPUT)" ] || (echo $$(OUTPUT) && exit 1)
