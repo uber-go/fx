e6cd4843ed0c2f14f881b4891e2a8741e82f35cc
anuptalwalkar <anup@uber.com>
2016-10-11 12:12:43 -0700

    Logger implementation with Zap (#64)
    * Added Logger implementation with zap
    
    * Added borrowed SugarLogger for use
    
    * Use akabos/zap SugarLogger instead of uber-go/zap
    
    * Remove borrowed SugarLogger implementation
    
    * Comments in proper format
    
    * Updated glide to use akabos/zap repo for uber-go/zap
    
    * Integrate zap.Logger and skip zap.Sugar
    
    * Mirror zap.Logger methods and imprved code coverage
    
    * Package name change to ulog

---
+++ core/luog/log.go
@@ -0,0 +1,254 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package ulog
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+
+	"github.com/uber-go/zap"
+)
+
+// Configuration for logging with uberfx
+type Configuration struct {
+	Level   string
+	File    *FileConfiguration
+	Stdout  bool
+	Verbose bool
+
+	prefixWithFileLine *bool `yaml:"prefix_with_fileline"`
+	TextFormatter      *bool // use TextFormatter (default json)
+}
+
+// FileConfiguration describes the properties needed to log to a file
+type FileConfiguration struct {
+	Enabled   bool
+	Directory string
+	FileName  string
+}
+
+type baselogger struct {
+	log        zap.Logger
+	initFields []interface{}
+}
+
+// Log is the Uberfx wrapper for underlying logging service
+type Log interface {
+	// Configure Log object with the provided log.Configuration
+	Configure(Configuration)
+	// Create a child logger with some context
+	With(...interface{}) Log
+	SetLevel(zap.Level)
+	Check(zap.Level, string) *zap.CheckedMessage
+
+	Log(zap.Level, string, ...interface{})
+	Debug(string, ...interface{})
+	Info(string, ...interface{})
+	Warn(string, ...interface{})
+	Error(string, ...interface{})
+	Panic(string, ...interface{})
+	Fatal(string, ...interface{})
+	DFatal(string, ...interface{})
+}
+
+var std = defaultLogger()
+
+func defaultLogger() Log {
+	return &baselogger{
+		log:        zap.New(zap.NewJSONEncoder()),
+		initFields: nil,
+	}
+}
+
+// Logger returns the package-level logger configured for the service
+func Logger(fields ...interface{}) Log {
+	return &baselogger{
+		log:        std.(*baselogger).log,
+		initFields: fields,
+	}
+}
+
+// Configure the package-level logger based on provided configuration
+func Configure(cfg Configuration) {
+	std.Configure(cfg)
+}
+
+// Configure the calling logger based on provided configuration
+func (l *baselogger) Configure(cfg Configuration) {
+	options := make([]zap.Option, 0, 3)
+	if cfg.Verbose {
+		options = append(options, zap.DebugLevel)
+	} else {
+		l.With(zap.String("Level", cfg.Level)).Debug("setting log level")
+		var lv zap.Level
+		err := lv.UnmarshalText([]byte(cfg.Level))
+		if err != nil {
+			l.With(zap.String("Level", cfg.Level)).Debug("cannot parse log level. setting to Debug as default")
+			options = append(options, zap.DebugLevel)
+		} else {
+			options = append(options, lv)
+		}
+	}
+	if cfg.File == nil || !cfg.File.Enabled {
+		options = append(options, zap.Output(zap.AddSync(ioutil.Discard)))
+	} else {
+		options = append(options, zap.Output(l.fileOutput(cfg.File, cfg.Stdout, cfg.Verbose)))
+	}
+
+	if cfg.TextFormatter != nil && *cfg.TextFormatter {
+		l.SetLogger(zap.New(zap.NewTextEncoder(), options...))
+		return
+	}
+	l.SetLogger(zap.New(zap.NewJSONEncoder(), options...))
+}
+
+func (l *baselogger) fileOutput(cfg *FileConfiguration, stdout bool, verbose bool) zap.WriteSyncer {
+	fileLoc := path.Join(cfg.Directory, cfg.FileName)
+	l.Debug("adding log file output to zap")
+	err := os.MkdirAll(cfg.Directory, os.FileMode(0755))
+	if err != nil {
+		l.Fatal("failed to create log directory: ", err)
+	}
+	file, err := os.OpenFile(fileLoc, os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.FileMode(0755))
+	if err != nil {
+		l.With(zap.Error(err)).Fatal("failed to open log file for writing.")
+	}
+	l.With(zap.String("filename", fileLoc)).Debug("logfile created successfully")
+	if verbose || stdout {
+		return zap.AddSync(io.MultiWriter(os.Stdout, file))
+	}
+	return zap.AddSync(file)
+}
+
+// SetLogger allows users to inject underlying zap.Logger. Use this
+// for test, or or if you fancy your own logging setup
+func (l *baselogger) SetLogger(log zap.Logger) {
+	l.log = log
+}
+
+// RawLogger returns underneath zap implementation for use
+func (l *baselogger) RawLogger() zap.Logger {
+	return l.log
+}
+
+func (l *baselogger) SetLevel(level zap.Level) {
+	l.log.SetLevel(level)
+}
+
+func (l *baselogger) With(fields ...interface{}) Log {
+	return &baselogger{
+		log:        l.log.With(l.compileLogFields(fields...)...),
+		initFields: l.initFields,
+	}
+}
+
+func (l *baselogger) Check(level zap.Level, expr string) *zap.CheckedMessage {
+	return l.log.Check(level, expr)
+}
+
+func (l *baselogger) Debug(msg string, args ...interface{}) {
+	l.Log(zap.DebugLevel, msg, args...)
+}
+
+func (l *baselogger) Info(msg string, args ...interface{}) {
+	l.Log(zap.InfoLevel, msg, args...)
+}
+
+func (l *baselogger) Warn(msg string, args ...interface{}) {
+	l.Log(zap.WarnLevel, msg, args...)
+}
+
+func (l *baselogger) Error(msg string, args ...interface{}) {
+	l.Log(zap.ErrorLevel, msg, args...)
+}
+
+func (l *baselogger) Panic(msg string, args ...interface{}) {
+	l.Log(zap.PanicLevel, msg, args...)
+}
+
+func (l *baselogger) Fatal(msg string, args ...interface{}) {
+	l.Log(zap.FatalLevel, msg, args...)
+}
+
+func (l *baselogger) DFatal(msg string, args ...interface{}) {
+	l.log.DFatal(msg, l.compileLogFields(args)...)
+}
+
+func (l *baselogger) Log(lvl zap.Level, msg string, args ...interface{}) {
+	if cm := l.Check(lvl, msg); cm.OK() {
+		cm.Write(l.compileLogFields(args...)...)
+	}
+}
+
+func (l *baselogger) compileLogFields(args ...interface{}) []zap.Field {
+	var fields []interface{}
+	fields = append(fields, l.initFields...)
+	fields = append(fields, args...)
+	return l.fieldsConversion(fields...)
+}
+
+func (l *baselogger) fieldsConversion(args ...interface{}) []zap.Field {
+	fields := make([]zap.Field, 0, len(args)/2)
+	if len(args)%2 != 0 {
+		fields = append(fields, zap.Error(fmt.Errorf("invalid number of arguments")))
+		return fields
+	}
+	for idx := 0; idx < len(args); idx += 2 {
+		if key, ok := args[idx].(string); ok {
+			key = args[idx].(string)
+			switch value := args[idx+1].(type) {
+			case bool:
+				fields = append(fields, zap.Bool(key, value))
+			case float64:
+				fields = append(fields, zap.Float64(key, value))
+			case int:
+				fields = append(fields, zap.Int(key, value))
+			case int64:
+				fields = append(fields, zap.Int64(key, value))
+			case uint:
+				fields = append(fields, zap.Uint(key, value))
+			case uint64:
+				fields = append(fields, zap.Uint64(key, value))
+			case uintptr:
+				fields = append(fields, zap.Uintptr(key, value))
+			case string:
+				fields = append(fields, zap.String(key, value))
+			case time.Time:
+				fields = append(fields, zap.Time(key, value))
+			case time.Duration:
+				fields = append(fields, zap.Duration(key, value))
+			case zap.LogMarshaler:
+				fields = append(fields, zap.Marshaler(key, value))
+			case fmt.Stringer:
+				fields = append(fields, zap.Stringer(key, value))
+			case error:
+				fields = append(fields, zap.Error(value))
+			default:
+				fields = append(fields, zap.Object(key, value))
+			}
+		}
+	}
+	return fields
+}
---
+++ core/luog/log_test.go
@@ -0,0 +1,264 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package ulog
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uber-go/zap"
+)
+
+type testBuffer struct {
+	bytes.Buffer
+}
+
+func (b *testBuffer) Sync() error {
+	return nil
+}
+
+func (b *testBuffer) Lines() []string {
+	output := strings.Split(b.String(), "\n")
+	return output[:len(output)-1]
+}
+
+func (b *testBuffer) Stripped() string {
+	return strings.TrimRight(b.String(), "\n")
+}
+
+func withInMemoryLogger(t *testing.T, opts []zap.Option, f func(zap.Logger, *testBuffer)) {
+	sink := &testBuffer{}
+	errSink := &testBuffer{}
+
+	allOpts := make([]zap.Option, 0, 3+len(opts))
+	allOpts = append(allOpts, zap.DebugLevel, zap.Output(sink), zap.ErrorOutput(errSink))
+	allOpts = append(allOpts, opts...)
+	f(zap.New(zap.NewJSONEncoder(zap.NoTime()), allOpts...), sink)
+}
+
+func TestSimpleLogger(t *testing.T) {
+	withInMemoryLogger(t, nil, func(zaplogger zap.Logger, buf *testBuffer) {
+		log := Logger()
+		log.(*baselogger).SetLogger(zaplogger)
+		log.Debug("debug message", "a", "b")
+		log.Info("info message", "c", "d")
+		log.Warn("warn message", "e", "f")
+		log.Error("error message", "g", "h")
+		assert.Equal(t, []string{
+			`{"level":"debug","msg":"debug message","a":"b"}`,
+			`{"level":"info","msg":"info message","c":"d"}`,
+			`{"level":"warn","msg":"warn message","e":"f"}`,
+			`{"level":"error","msg":"error message","g":"h"}`,
+		}, buf.Lines(), "Incorrect output from logger")
+	})
+}
+
+func TestLoggerWithInitFields(t *testing.T) {
+	withInMemoryLogger(t, nil, func(zaplogger zap.Logger, buf *testBuffer) {
+		log := Logger("method", "test_method")
+		log.(*baselogger).SetLogger(zaplogger)
+
+		log.Debug("debug message", "a", "b")
+		log.Info("info message", "c", "d")
+		log.Warn("warn message", "e", "f")
+		log.Error("error message", "g", "h")
+		assert.Equal(t, []string{
+			`{"level":"debug","msg":"debug message","method":"test_method","a":"b"}`,
+			`{"level":"info","msg":"info message","method":"test_method","c":"d"}`,
+			`{"level":"warn","msg":"warn message","method":"test_method","e":"f"}`,
+			`{"level":"error","msg":"error message","method":"test_method","g":"h"}`,
+		}, buf.Lines(), "Incorrect output from logger")
+	})
+}
+
+func TestLoggerWithInvalidFields(t *testing.T) {
+	withInMemoryLogger(t, nil, func(zaplogger zap.Logger, buf *testBuffer) {
+		log := Logger()
+		log.(*baselogger).SetLogger(zaplogger)
+		log.Info("info message", "c")
+		log.Info("info message", "c", "d", "e")
+		log.DFatal("debug message")
+		assert.Equal(t, []string{
+			`{"level":"info","msg":"info message","error":"invalid number of arguments"}`,
+			`{"level":"info","msg":"info message","error":"invalid number of arguments"}`,
+			`{"level":"error","msg":"debug message","error":"invalid number of arguments"}`,
+		}, buf.Lines(), "Incorrect output from logger")
+	})
+}
+
+func TestFatalsAndPanics(t *testing.T) {
+	withInMemoryLogger(t, nil, func(zaplogger zap.Logger, buf *testBuffer) {
+		log := Logger()
+		log.(*baselogger).SetLogger(zaplogger)
+		assert.Panics(t, func() { log.Panic("panic level") }, "Expected logging at Panic level to panic.")
+		assert.Equal(t, `{"level":"panic","msg":"panic level"}`, buf.Stripped(), "Unexpected output from panic-level Log.")
+	})
+
+}
+
+func TestConfiguredLogger(t *testing.T) {
+	withLogger(t, func(tmpDir string, logFile string) {
+		log := Logger()
+		txt := false
+		cfg := Configuration{
+			Level:         "debug",
+			Stdout:        false,
+			TextFormatter: &txt,
+			Verbose:       false,
+		}
+		log.Configure(cfg)
+		zaplogger := log.(*baselogger).RawLogger()
+		assert.Equal(t, zap.DebugLevel, zaplogger.Level())
+	})
+}
+
+func TestConfiguredLoggerWithTextFormatter(t *testing.T) {
+	withLogger(t, func(tmpDir string, logFile string) {
+		log := Logger()
+		txt := true
+		cfg := Configuration{
+			Level:         "debug",
+			Stdout:        false,
+			TextFormatter: &txt,
+			Verbose:       false,
+			File: &FileConfiguration{
+				Directory: tmpDir,
+				FileName:  logFile,
+				Enabled:   true,
+			},
+		}
+		log.Configure(cfg)
+		zaplogger := log.(*baselogger).RawLogger()
+		assert.Equal(t, zap.DebugLevel, zaplogger.Level())
+	})
+}
+
+func TestConfiguredLoggerWithStdout(t *testing.T) {
+	withLogger(t, func(tmpDir string, logFile string) {
+		log := Logger()
+		txt := false
+		cfg := Configuration{
+			Stdout:        true,
+			TextFormatter: &txt,
+			Verbose:       true,
+			File: &FileConfiguration{
+				Enabled:   true,
+				Directory: tmpDir,
+				FileName:  logFile,
+			},
+		}
+		log.Configure(cfg)
+		zaplogger := log.(*baselogger).RawLogger()
+		assert.Equal(t, zap.DebugLevel, zaplogger.Level())
+	})
+
+}
+
+func withLogger(t *testing.T, f func(string, string)) {
+	tmpDir, err := ioutil.TempDir("", "default_log")
+	defer os.RemoveAll(tmpDir)
+	require.NoError(t, err)
+
+	tmpFile, err := ioutil.TempFile(tmpDir, "temp_log.txt")
+	require.NoError(t, err)
+	logFile, err := filepath.Rel(tmpDir, tmpFile.Name())
+	require.NoError(t, err)
+	txt := false
+	cfg := Configuration{
+		Level:         "error",
+		Stdout:        false,
+		TextFormatter: &txt,
+		Verbose:       false,
+		File: &FileConfiguration{
+			Enabled:   true,
+			Directory: tmpDir,
+			FileName:  logFile,
+		},
+	}
+	Configure(cfg)
+	f(tmpDir, logFile)
+}
+
+func TestDefaultPackageLogger(t *testing.T) {
+	withLogger(t, func(tmpDir string, logFile string) {
+		logger := Logger()
+		zaplogger := logger.(*baselogger).RawLogger()
+		assert.Equal(t, zap.ErrorLevel, zaplogger.Level())
+		logger.SetLevel(zap.WarnLevel)
+		assert.Equal(t, zap.WarnLevel, zaplogger.Level())
+	})
+}
+
+func TestDefaultLoggingWithInitFields(t *testing.T) {
+	withLogger(t, func(tmpDir string, logFile string) {
+		logger := Logger("a", "b")
+		logger.Error("test log")
+		content, err := ioutil.ReadFile(filepath.Join(tmpDir, logFile))
+		require.NoError(t, err)
+		assert.True(t, strings.Contains(string(content), "test log"))
+		assert.True(t, strings.Contains(string(content), `"a":"b"`))
+	})
+}
+
+type marshalObject struct {
+	Data string `json:"data"`
+}
+
+func (m *marshalObject) MarshalLog(kv zap.KeyValue) error {
+	kv.AddString("Data", m.Data)
+	return nil
+}
+
+func TestFieldConversion(t *testing.T) {
+	log := Logger()
+	assert.Equal(t, zap.Bool("a", true), log.(*baselogger).fieldsConversion("a", true)[0])
+	assert.Equal(t, zap.Float64("a", 5.5), log.(*baselogger).fieldsConversion("a", 5.5)[0])
+	assert.Equal(t, zap.Int("a", 10), log.(*baselogger).fieldsConversion("a", 10)[0])
+	assert.Equal(t, zap.Int64("a", int64(10)), log.(*baselogger).fieldsConversion("a", int64(10))[0])
+	assert.Equal(t, zap.Uint("a", uint(10)), log.(*baselogger).fieldsConversion("a", uint(10))[0])
+	assert.Equal(t, zap.Uintptr("a", uintptr(0xa)), log.(*baselogger).fieldsConversion("a", uintptr(0xa))[0])
+	assert.Equal(t, zap.Uint64("a", uint64(10)), log.(*baselogger).fieldsConversion("a", uint64(10))[0])
+	assert.Equal(t, zap.String("a", "xyz"), log.(*baselogger).fieldsConversion("a", "xyz")[0])
+	assert.Equal(t, zap.Time("a", time.Unix(0, 0)), log.(*baselogger).fieldsConversion("a", time.Unix(0, 0))[0])
+	assert.Equal(t, zap.Duration("a", time.Microsecond), log.(*baselogger).fieldsConversion("a", time.Microsecond)[0])
+	dt := &marshalObject{Data: "value"}
+	assert.Equal(t, zap.Marshaler("a", &marshalObject{"value"}), log.(*baselogger).fieldsConversion("a", dt)[0])
+	ip := net.ParseIP("1.2.3.4")
+	assert.Equal(t, zap.Stringer("ip", ip), log.(*baselogger).fieldsConversion("ip", ip)[0])
+	assert.Equal(t, zap.Object("a", []int{1, 2}), log.(*baselogger).fieldsConversion("a", []int{1, 2})[0])
+	err := fmt.Errorf("test error")
+	assert.Equal(t, zap.Error(err), log.(*baselogger).fieldsConversion("error", err)[0])
+
+}
+
+func TestRawLogger(t *testing.T) {
+	log := Logger()
+	assert.NotNil(t, log.(*baselogger).RawLogger())
+}
