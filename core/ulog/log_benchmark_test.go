187114d2c830076a1022861a347c094e79d70fa6
Anup Talwalkar <anup@uber.com>
2016-11-09 16:13:08 -0800

    Added benchmarks for logging (#54)
    * Added benchmarks for logging
    
    * Refactored test with discardedLogger() func
    
    * Updated benchmarks with different callers

---
+++ core/ulog/log_benchmark_test.go
@@ -0,0 +1,133 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package ulog
+
+import (
+	"testing"
+	"time"
+
+	"github.com/uber-go/zap"
+)
+
+func discardedLogger() zap.Logger {
+	return zap.New(
+		zap.NewJSONEncoder(),
+		zap.DiscardOutput,
+	)
+}
+
+func BenchmarkUlogWithoutFields(b *testing.B) {
+	log := Logger()
+	log.SetLogger(discardedLogger())
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("Ulog message")
+		}
+	})
+}
+
+func BenchmarkUlogWithFieldsLogIFace(b *testing.B) {
+	log := Logger()
+	log.SetLogger(discardedLogger())
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("Ulog message",
+				"int", 123,
+				"int64", 123,
+				"float", 123.123,
+				"string", "four!",
+				"bool", true,
+				"time", time.Unix(0, 0),
+				"duration", time.Second,
+				"another string", "done!")
+		}
+	})
+}
+
+func BenchmarkUlogWithFieldsBaseLoggerStruct(b *testing.B) {
+	log := Logger()
+	log.SetLogger(discardedLogger())
+	base := log.(*baselogger)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			base.Info("Ulog message",
+				"int", 123,
+				"int64", 123,
+				"float", 123.123,
+				"string", "four!",
+				"bool", true,
+				"time", time.Unix(0, 0),
+				"duration", time.Second,
+				"another string", "done!")
+		}
+	})
+}
+
+func BenchmarkUlogWithFieldsZapLogger(b *testing.B) {
+	log := discardedLogger()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("Ulog message",
+				zap.Int("int", 123),
+				zap.Int64("int64", 123),
+				zap.Float64("float", 123.123),
+				zap.String("string", "four!"),
+				zap.Bool("bool", true),
+				zap.Time("time", time.Unix(0, 0)),
+				zap.Duration("duration", time.Second),
+				zap.String("another string", "done!"))
+		}
+	})
+}
+
+func BenchmarkUlogLiteWithFields(b *testing.B) {
+	log := Logger()
+	log.SetLogger(discardedLogger())
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("Ulog message", "integer", 123, "string", "string")
+		}
+	})
+}
+
+func BenchmarkUlogWithFieldsPreset(b *testing.B) {
+	log := Logger(
+		"int", 123,
+		"int64", 123,
+		"float", 123.123,
+		"string", "four!",
+		"bool", true,
+		"time", time.Unix(0, 0),
+		"duration", time.Second,
+		"another string", "done!")
+	log.SetLogger(discardedLogger())
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("Ulog message")
+		}
+	})
+}
