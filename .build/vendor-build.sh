95d7bc031eb22b4fc89592cbf1b89f2c1aeeb563
Abhinav Gupta <abg@uber.com>
2016-12-20 12:58:38 -0800

    vendor-build: Work without realpath
    The realpath command isn't available on Travis

--- .build/vendor-build.sh
+++ .build/vendor-build.sh
@@ -17,8 +17,12 @@ if [[ ! -d vendor ]]; then
 fi
 
 function die() {
-	echo "$1"
+	echo "$1" >&2
 	exit 1
+}
+
+function abspath() {
+	(cd "$1" || die "Directory $1 does not exist"; pwd)
 }
 
 # findGlideLock dir looks for glide.lock in dir or any of its parent
@@ -35,7 +39,7 @@ function findGlideLock() {
 		return
 	fi
 
-	findGlideLock "$(realpath --no-symlinks "$1/..")"
+	findGlideLock "$(abspath "$1/..")"
 }
 
 outputDir="$1"
