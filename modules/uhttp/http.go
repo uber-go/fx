898382bf30d793de55f8d89827d653e9b39108cf
Yutong Pei <yutongp@gmail.com>
2017-02-15 22:51:54 -0800

    Debug use default `true`

--- modules/uhttp/http.go
+++ modules/uhttp/http.go
@@ -78,7 +78,7 @@ type Config struct {
 	modules.ModuleConfig
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
-	Debug   bool          `yaml:"debug"`
+	Debug   bool          `yaml:"debug" default:"true"`
 }
 
 // GetHandlersFunc returns a slice of registrants from a service host
