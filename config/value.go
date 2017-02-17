61a1f1451cff85587cf80ce33e04992689d1af03
Yutong Pei <yutongp@gmail.com>
2017-02-17 09:46:05 -0800

    add test to ensure config does not panic when populate for Array type (#279)
    add a test to ensure we don’t panic when populate config for Array type and a TODO to Alex

--- config/value.go
+++ config/value.go
@@ -292,6 +292,7 @@ const (
 	bucketArray     = 1
 	bucketObject    = 2
 	bucketMap       = 3
+	bucketSlice     = 4
 )
 
 func getBucket(t reflect.Type) int {
@@ -309,9 +310,9 @@ func getBucket(t reflect.Type) int {
 	case reflect.Map:
 		return bucketMap
 	case reflect.Array:
-		fallthrough
-	case reflect.Slice:
 		return bucketArray
+	case reflect.Slice:
+		return bucketSlice
 	case reflect.Struct:
 		return bucketObject
 	}
@@ -531,6 +532,8 @@ func (cv Value) valueStruct(key string, target interface{}) (interface{}, error)
 				fieldValue.Set(newTarget)
 			}
 		case bucketArray:
+			// TODO(alsam) fix array type DRI-12.
+		case bucketSlice:
 			destSlice := reflect.MakeSlice(fieldType, 0, 4)
 
 			// start looking for child values.
