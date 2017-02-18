c877139835c9d24c7271f7fddc54c2a786a469f5
Akshay Shah <shah@uber.com>
2017-02-17 23:36:14 -0800

    Update ulog to the latest zap
    Remove most of ulog, since it's either unnecessary or subsumed by zap.
    The only lost functionality here is Sentry integration, which we can
    bring back next week (either as an UberFX-specific encoder wrapper, or
    as a Filebeat plugin).

--- ulog/metrics_test.go
+++ ulog/metrics_test.go
@@ -20,13 +20,40 @@
 
 package ulog
 
-import "github.com/uber-go/zap"
+import (
+	"testing"
 
-func nopLogger() Log {
-	return &baseLogger{
-		log: zap.New(zap.NullEncoder()),
-	}
+	"go.uber.org/fx/testutils/metrics"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestMetricsHook(t *testing.T) {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.OutputPaths = nil
+	cfg.ErrorOutputPaths = nil
+	log, err := cfg.Build()
+	require.NoError(t, err, "Failed to construct a logger.")
+
+	scope, reporter := metrics.NewTestScope()
+	log = log.WithOptions(zap.Hooks(Metrics(scope)))
+
+	reporter.CountersWG.Add(6)
+	log.Debug("debug")
+	log.Info("info")
+	log.Warn("warn")
+	log.Error("error")
+	assert.Panics(t, func() { log.DPanic("dpanic") }, "Expected Logger.DPanic to panic in development.")
+	assert.Panics(t, func() { log.Panic("panic") }, "Expected Logger.Panic to panic.")
+	reporter.CountersWG.Wait()
+
+	// FX's metrics spy doesn't support tags, so granular assertions aren't
+	// possible.
+	assert.Equal(t, 1, len(reporter.Counters))
+	assert.Equal(t, int64(6), reporter.Counters["logs"], "Expected counts to have name:logs.")
 }
 
-// NopLogger should be used in tests if you wish to discard the output
-var NopLogger = nopLogger()
+func assertConfigEqual(t testing.TB, merged, expected zap.Config) {
+}
