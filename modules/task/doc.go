9170301f575d62430d3120176d6708ece848ed10
Peter Edge <pedge@uber.com>
2017-03-02 09:37:05 +0100

    Make gendoc

--- modules/task/doc.go
+++ modules/task/doc.go
@@ -49,7 +49,7 @@
 //
 //   func main() {
 //     svc, err := service.WithModule(
-//       "example",
+//       "task",
 //       task.New(newBackend),
 //     ).Build()
 //     if err := task.Register(updateCache); err != nil {
