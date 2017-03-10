595add41907f16d43f15071498752d6d0bd859df
Peter Edge <pedge@uber.com>
2017-03-10 16:03:25 -0500

    Speed up make lint

--- .build/flags.mk
+++ .build/flags.mk
@@ -3,7 +3,9 @@ PKGS ?= $(shell glide novendor)
 LIST_PKGS ?= $(shell go list ./... | grep -v /vendor/)
 
 # Many Go tools take file globs or directories as arguments instead of packages.
-PKG_FILES ?= *.go auth config dig internal metrics modules service testutils tracing ulog
+ROOT_PKG_FILES := $(wildcard *.go)
+PKG_NAMES := auth config dig internal metrics modules service testutils tracing ulog
+PKG_FILES ?= $(ROOT_PACKAGE_FILES) $(PKG_NAMES)
 
 # The linting tools evolve with each Go version, so run them only on the latest
 # stable release.
