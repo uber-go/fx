2e34a06630233ccb7219c5eb285eecac6e8f7680
Alex Samylkin <alsam@uber.com>
2017-03-11 07:01:54 -0800

    Check if a value is addressable

--- config/decoder.go
+++ config/decoder.go
@@ -318,6 +318,10 @@ func (d *decoder) textUnmarshaller(key string, value reflect.Value, str string) 
 	}
 
 	// Value has to have a pointer receiver to be able to modify itself with TextUnmarshaller
+	if !value.CanAddr() {
+		return fmt.Errorf("can't use TextUnmarshaller because %s is not addressable", key)
+	}
+
 	switch t := value.Addr().Interface().(type) {
 	case encoding.TextUnmarshaler:
 		return t.UnmarshalText([]byte(str))
