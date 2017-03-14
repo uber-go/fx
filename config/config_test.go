e8b782f60fc93bcc0f0a458691b38654044dd011
Alex <alsam@uber.com>
2017-03-14 12:13:46 -0700

    Add nop config provider (#409)
    If users want to test a specific functionality, 
    e.g. RegisterCallback always returns an error, 
    the easiest way to do that will be embedding 
    a NopConfigProvier, and they can't do this 
    with a staticProvider, because it is private.
    
    Another part is type preservation: in static provider
    we serialize objects to yaml and loose type information, 
    uints, int8s will be converted to ints, float32s to float64.

--- config/config_test.go
+++ config/config_test.go
@@ -409,3 +409,16 @@ func TestEnvProviderWithEmptyPrefix(t *testing.T) {
 	scope := emptyScope.Get("key")
 	require.Equal(t, "value", scope.Get("").AsString())
 }
+
+func TestNopProvider_Get(t *testing.T) {
+	t.Parallel()
+	p := NopProvider{}
+	assert.Equal(t, "NopProvider", p.Name())
+	assert.NoError(t, p.RegisterChangeCallback("key", nil))
+	assert.NoError(t, p.UnregisterChangeCallback("token"))
+
+	v := p.Get("randomKey")
+	assert.Equal(t, "NopProvider", v.Source())
+	assert.True(t, v.HasValue())
+	assert.Nil(t, v.Value())
+}
