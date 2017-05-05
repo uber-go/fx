159b8da3aa2d5e96fc065ecab2ca89c953523ad4
Alexandr Samylkin <alsam@uber.com>
2017-05-04 18:20:11 -0700

    gofmt

--- modules/yarpc/yarpc.go
+++ modules/yarpc/yarpc.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package yarpc
 
 import (
@@ -14,18 +34,22 @@ import (
 	"go.uber.org/zap"
 )
 
+// Module represents a collection of YARPC components.
 type Module struct {
 	l           *zap.Logger
 	handlerCtor fx.Component
 	d           *yarpc.Dispatcher
 }
 
+// Transports represent a collection Procedures that will be registered with a dispatcher.
 type Transports struct {
+	//Ts - transports to be registered.
 	Ts []transport.Procedure
 }
 
 type starter struct{}
 
+// New creates a new YARPC Module with a handler constructor.
 func New(handlerCtor fx.Component) *Module {
 	if handlerCtor == nil {
 		panic("Expect a non nil handler constructor")
@@ -34,6 +58,7 @@ func New(handlerCtor fx.Component) *Module {
 	return &Module{handlerCtor: handlerCtor}
 }
 
+// Name returns yarpc
 func (m *Module) Name() string {
 	return "yarpc"
 }
@@ -46,6 +71,7 @@ func (m *Module) populateConfig(provider config.Provider) (yarpc.Config, error) 
 	return cfg.LoadConfig(provider.Get("name").AsString(), val)
 }
 
+// Constructor returns Module components.
 func (m *Module) Constructor() []fx.Component {
 	return []fx.Component{
 		func(provider config.Provider, scope tally.Scope, logger *zap.Logger) (*yarpc.Dispatcher, error) {
@@ -73,6 +99,7 @@ func (m *Module) Constructor() []fx.Component {
 	}
 }
 
+// Stop stops the dispatcher.
 func (m *Module) Stop() error {
 	if err := m.d.Stop(); err != nil {
 		m.l.Error("Failed to stop dispatcher", zap.Error(err))
