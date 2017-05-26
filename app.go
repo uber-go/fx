64474d4fdaef08553d4764579561822f15a08399
Grayson Koonce <grayson@uber.com>
2017-05-25 19:31:45 -0700

    clean stmt

--- app.go
+++ app.go
@@ -122,7 +122,7 @@ func (s *App) start(funcs ...interface{}) error {
 			return errors.Errorf("%T %q is not a function", fn, fn)
 		}
 
-		logf("CALL\t%s @ %s", fnName(fn), fnLoc(fn))
+		logf("CALL\tfunc %s @ %s", fnName(fn), fnLoc(fn))
 
 		if err := s.container.Invoke(fn); err != nil {
 			return err
