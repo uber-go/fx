05ec8ab422f93ac75db6232aa07efb6ba6c95b80
Grayson Koonce <grayson@uber.com>
2017-05-25 22:20:18 -0700

    add lifecycle to log output

--- lifecycle.go
+++ lifecycle.go
@@ -35,6 +35,7 @@ type Lifecycle interface {
 type Hook struct {
 	OnStart func() error
 	OnStop  func() error
+	caller  string
 }
 
 func newLifecycle() Lifecycle {
@@ -47,6 +48,7 @@ type lifecycle struct {
 }
 
 func (l *lifecycle) Append(hook Hook) {
+	hook.caller = caller()
 	l.hooks = append(l.hooks, hook)
 }
 
@@ -56,7 +58,7 @@ func (l *lifecycle) Append(hook Hook) {
 func (l *lifecycle) start() error {
 	for i, hook := range l.hooks {
 		if hook.OnStart != nil {
-			logf("START\t\t")
+			logf("START\t\t%s", hook.caller)
 			if err := hook.OnStart(); err != nil {
 				return err
 			}
@@ -79,7 +81,7 @@ func (l *lifecycle) stop() error {
 		if l.hooks[i].OnStop == nil {
 			continue
 		}
-		logf("STOP\t\t")
+		logf("STOP\t\t%s", l.hooks[i].caller)
 		if err := l.hooks[i].OnStop(); err != nil {
 			errs = append(errs, err)
 		}
--- logging.go
+++ logging.go
@@ -59,3 +59,23 @@ func mainPath() string {
 	}
 	return path.Dir(filename)
 }
+
+func caller() string {
+	// we get the callers as uintptrs - but we just need 1
+	fpcs := make([]uintptr, 1)
+
+	// skip 3 levels to get to the caller of whoever called Caller()
+	n := runtime.Callers(3, fpcs)
+	if n == 0 {
+		return "n/a" // proper error her would be better
+	}
+
+	// get the info of the actual function that's in the pointer
+	fun := runtime.FuncForPC(fpcs[0] - 1)
+	if fun == nil {
+		return "n/a"
+	}
+
+	// return its name
+	return fun.Name()
+}
