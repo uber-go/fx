bd885d3b073d94cdc1740a71fd9774833d6ee28b
Grayson Koonce <grayson@uber.com>
2017-07-20 14:10:49 -0700

    take less loc

--- inout.go
+++ inout.go
@@ -8,9 +8,7 @@ import "go.uber.org/dig"
 // Modules should take a single param struct that embeds an In
 // in order to provide a forwards-compatible API where additional
 // optional properties can be added without breaking.
-type In struct {
-	dig.In
-}
+type In struct{ dig.In }
 
 // Out can be embedded in return structs in order to
 // name types and provide
@@ -18,6 +16,4 @@ type In struct {
 // Modules should return a single results struct that embeds
 // an Out in order to provide a forwards-compatible API where
 // additional types can be provided over time without breaking.
-type Out struct {
-	dig.Out
-}
+type Out struct{ dig.Out }
