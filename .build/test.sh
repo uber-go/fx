197a55c11d65d9f35f19d6a2f5214866ab669453
Yuri Shkuro <ys@uber.com>
2017-09-20 15:05:45 -0400

    Measure timing of `go test` commands

--- .build/test.sh
+++ .build/test.sh
@@ -5,7 +5,7 @@ echo "" > coverage.txt
 
 # aggregate coverage from multiple packages
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic "$d"
+    time go test -race -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
