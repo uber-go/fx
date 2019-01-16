8cd613170ec01b1ca567ff0af4ad613746f39e01
Abhinav Gupta <abg@uber.com>
2019-01-15 22:12:44 -0800

    docs/Done: Add period

--- app.go
+++ app.go
@@ -450,8 +450,9 @@ func (app *App) Stop(ctx context.Context) error {
 // application. Applications listen for the SIGINT and SIGTERM signals; during
 // development, users can send the application SIGTERM by pressing Ctrl-C in
 // the same terminal as the running process.
+//
 // Alternatively, a signal can be broadcast to all done channels manually by
-// using the Shutdown functionality (see the Shutdowner documentation for details)
+// using the Shutdown functionality (see the Shutdowner documentation for details).
 func (app *App) Done() <-chan os.Signal {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
