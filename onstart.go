1ee82474f49e743b0b09449a1f1effda1ba2f173
Prashant Varanasi <prashant@uber.com>
2019-07-11 01:14:26 -0700

    Allow OnStart to return Stopper, add tests

--- onstart.go
+++ onstart.go
@@ -26,9 +26,22 @@ import (
 	"reflect"
 )
 
-var _ctxType = typeOfInterface(new(context.Context))
-var _lifecycleType = typeOfInterface(new(Lifecycle))
-var _errType = typeOfInterface(new(error))
+var (
+	_ctxType       = typeOfInterface(new(context.Context))
+	_lifecycleType = typeOfInterface(new(Lifecycle))
+	_errType       = typeOfInterface(new(error))
+	_stopperType   = reflect.TypeOf(Stopper(nil))
+)
+
+// Stopper functions are are executed when the applicaton is stopped.
+// They can be returned from a function passed in to OnStart.
+type Stopper func(context.Context) error
+
+type onStartType struct {
+	hasCtx     bool
+	hasErr     bool
+	hasStopper bool
+}
 
 func typeOfInterface(newIfacePtr interface{}) reflect.Type {
 	return reflect.TypeOf(newIfacePtr).Elem()
@@ -45,6 +58,29 @@ func typeOfInterface(newIfacePtr interface{}) reflect.Type {
 //
 // If the first argument of the passed in function is a context.Context, then the
 // context from Start is passed in.
+//
+// If any of OnStart functions return a Stopper as their last non-error
+// argument, the Stopper will be queued up with OnStop.
+//
+//  fx.New(
+//    ...,
+//    OnStart(func(server *http.Server) Stopper {
+//      go server.ListenAndServe()
+//      return server.Shutdown
+//    }),
+//  )
+//
+// The above is equivalent to,
+//
+//  fx.New(
+//    ...,
+//    OnStart(func(server *http.Server) {
+//      go server.ListenAndServe()
+//    }),
+//    OnStop(func(ctx context.Context, server *http.Server) error {
+//      return server.Shutdown(ctx)
+//    }),
+//  )
 func OnStart(funcs ...interface{}) Option {
 	var opts []Option
 	for _, f := range funcs {
@@ -53,39 +89,52 @@ func OnStart(funcs ...interface{}) Option {
 	return Options(opts...)
 }
 
-func validateOnStartFunc(ft reflect.Type) error {
+func validateOnStartFunc(ft reflect.Type) (onStartType, error) {
 	if kind := ft.Kind(); kind != reflect.Func {
-		return fmt.Errorf("OnStart must be passed a function, got %v", kind)
+		return onStartType{}, fmt.Errorf("OnStart must be passed a function, got %v", kind)
 	}
+
+	onStartDesc := onStartType{
+		hasCtx: ft.NumIn() > 0 && ft.In(0) == _ctxType,
+	}
+
 	switch ft.NumOut() {
 	case 0:
 		// No return value, nothing to validate.
 	case 1:
-		// Must be error
-		if ft.Out(0) != _errType {
-			return fmt.Errorf("OnStart functions can only return an error, found %v", ft.Out(0))
+		switch ft.Out(0) {
+		case _errType:
+			onStartDesc.hasErr = true
+		case _stopperType:
+			onStartDesc.hasStopper = true
+		default:
+			return onStartDesc, fmt.Errorf("OnStart functions can only return an error or a Stopper, found %v", ft.Out(0))
 		}
+	case 2:
+		if ft.Out(0) != _stopperType || ft.Out(1) != _errType {
+			return onStartDesc, fmt.Errorf("OnStart functions can only return (Stopper, error), found (%v, %v)", ft.Out(0), ft.Out(1))
+		}
+		onStartDesc.hasErr = true
+		onStartDesc.hasStopper = true
 	default:
-		return fmt.Errorf("OnStart functions must have no returns, or a single error return")
+		return onStartDesc, fmt.Errorf("OnStart functions must not have more than 2 returns")
 	}
 
-	return nil
+	return onStartDesc, nil
 }
 
 func onStart(f reflect.Value) Option {
 	ft := f.Type()
 
-	if err := validateOnStartFunc(ft); err != nil {
+	onStartDesc, err := validateOnStartFunc(ft)
+	if err != nil {
 		return invokeErr(err)
 	}
-
-	// If the first argument is a context, ignore it, but track we need it for later.
-	isFirstCtx := ft.NumIn() > 0 && ft.In(0) == _ctxType
 
 	// The invoke function will take a lifecycle, followed by any arguments to f.
 	invokeInTypes := []reflect.Type{_lifecycleType}
 	for i := 0; i < ft.NumIn(); i++ {
-		if i == 0 && isFirstCtx {
+		if i == 0 && onStartDesc.hasCtx {
 			continue
 		}
 
@@ -97,17 +146,38 @@ func onStart(f reflect.Value) Option {
 	invokeFunc := reflect.MakeFunc(invokeFuncType, func(args []reflect.Value) []reflect.Value {
 		// The first argument to invoke is a lifecycle, let's extract that.
 		lifecycle := args[0].Interface().(Lifecycle)
+		var onStop Stopper
 		lifecycle.Append(Hook{
 			OnStart: func(ctx context.Context) error {
 				var startArgs []reflect.Value
 
-				if isFirstCtx {
+				if onStartDesc.hasCtx {
 					startArgs = append(startArgs, reflect.ValueOf(ctx))
 				}
 
 				startArgs = append(startArgs, args[1:]...)
-				f.Call(startArgs)
+				res := f.Call(startArgs)
+
+				// If the OnStop returns an error, then it must be the last argument
+				if onStartDesc.hasErr {
+					if err := res[len(res)-1].Interface(); err != nil {
+						return err.(error)
+					}
+				}
+
+				// If the OnStop returns a Stopper, it must be the first argument.
+				if onStartDesc.hasStopper {
+					onStop = res[0].Interface().(Stopper)
+				}
+
 				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				if onStop == nil {
+					return nil
+				}
+
+				return onStop(ctx)
 			},
 		})
 
--- onstart_test.go
+++ onstart_test.go
@@ -22,6 +22,7 @@ package fx
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"testing"
 
@@ -29,9 +30,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOnStart(t *testing.T) {
+func TestOnStartSuccess(t *testing.T) {
 	type A string
 	type B string
+	type ctxKey struct{}
 
 	type Params struct {
 		In
@@ -41,6 +43,13 @@ func TestOnStart(t *testing.T) {
 	}
 
 	var called []string
+
+	ctxWithVal := func(val string) context.Context {
+		return context.WithValue(context.Background(), ctxKey{}, val)
+	}
+	ctxVal := func(ctx context.Context) interface{} {
+		return ctx.Value(ctxKey{})
+	}
 
 	// No arguments
 	noArgs := func() {
@@ -55,6 +64,20 @@ func TestOnStart(t *testing.T) {
 	hasParams := func(p Params) {
 		called = append(called, fmt.Sprintf("hasParams(%v,%v)", p.A, p.B))
 	}
+	hasStopper := func(a A) Stopper {
+		called = append(called, fmt.Sprintf("hasStopper(%v)", a))
+		return func(ctx context.Context) error {
+			called = append(called, fmt.Sprintf("hasStopper-stopper(%v)", a))
+			return nil
+		}
+	}
+	hasStopperErr := func(ctx context.Context, b B) (Stopper, error) {
+		called = append(called, fmt.Sprintf("hasStopperErr(%v,%v)", ctxVal(ctx), b))
+		return func(ctx context.Context) error {
+			called = append(called, fmt.Sprintf("hasStopperErr-stopper(%v,%v)", ctxVal(ctx), b))
+			return nil
+		}, nil
+	}
 
 	app := New(
 		Provide(
@@ -65,16 +88,101 @@ func TestOnStart(t *testing.T) {
 		OnStart(noArgs, hasArgs),
 		OnStart(hasCtxAndArgs),
 		OnStart(hasParams),
+		OnStart(hasStopper),
+		OnStart(hasStopperErr),
 	)
+
+	startCtx := ctxWithVal("start-Ctx")
+	stopCtx := ctxWithVal("stop-Ctx")
+
 	require.NoError(t, app.Err(), "Failed to create app")
-	err := app.Start(context.Background())
-	require.NoError(t, err)
+	require.NoError(t, app.Start(startCtx))
+	require.NoError(t, app.Stop(stopCtx))
 
 	want := []string{
 		"noArgs",
 		"hasArgs(val-A,val-B)",
 		"hasCtxAndArgs(true,val-A)",
 		"hasParams(val-A,val-B)",
+		"hasStopper(val-A)",
+		"hasStopperErr(start-Ctx,val-B)",
+
+		// Stops happen at the end in inverse order to start.
+		"hasStopperErr-stopper(stop-Ctx,val-B)",
+		"hasStopper-stopper(val-A)",
 	}
 	assert.Equal(t, want, called)
+}
+
+func TestOnStartError(t *testing.T) {
+	t.Run("OnStart returns error", func(t *testing.T) {
+		onStart := func() error {
+			return errors.New("onStart failed")
+		}
+		app := New(
+			OnStart(onStart),
+		)
+
+		err := app.Start(context.Background())
+		require.Error(t, err, "Start should fail due to OnStart error")
+		assert.Contains(t, err.Error(), "onStart failed")
+	})
+
+	t.Run("OnStop returns error", func(t *testing.T) {
+		onStart := func() (Stopper, error) {
+			return func(context.Context) error {
+				return errors.New("onStop failed")
+			}, nil
+		}
+		app := New(
+			OnStart(onStart),
+		)
+
+		require.NoError(t, app.Start(context.Background()), "Start should not fail")
+
+		err := app.Stop(context.Background())
+		require.Error(t, err, "Start should fail due to OnStart error")
+		assert.Contains(t, err.Error(), "onStop failed")
+	})
+}
+
+func TestOnStartInvalidType(t *testing.T) {
+	tests := []struct {
+		msg     string
+		onStart interface{}
+		wantErr string
+	}{
+		{
+			msg:     "ctx is not first arg and not in graph",
+			onStart: func(s string, ctx context.Context) {},
+			wantErr: "type context.Context is not in the container",
+		},
+		{
+			msg:     "onStart returns single unsupported type only",
+			onStart: func() string { return "s" },
+			wantErr: "only return an error or a Stopper",
+		},
+		{
+			msg:     "onStart returns unsupported type and error",
+			onStart: func() (string, error) { return "s", nil },
+			wantErr: "only return (Stopper, error)",
+		},
+		{
+			msg:     "onStart returns 3 results",
+			onStart: func() (Stopper, string, error) { return nil, "s", nil },
+			wantErr: "more than 2 returns",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.msg, func(t *testing.T) {
+			app := New(
+				Provide(func() string { return "str" }),
+				OnStart(tt.onStart),
+			)
+			err := app.Err()
+			require.Error(t, err, "Should fail to create app")
+			assert.Contains(t, err.Error(), tt.wantErr, "Unexpected error")
+		})
+	}
 }
