a185e10e0eb565012f9c31fbdf4553f283c2411a
Abhinav Gupta <abg@uber.com>
2022-10-12 10:39:11 -0700

    docs: Add Concepts/Application Lifecycle
    This adds a page documenting the application lifecycle at a high level.
    This does not go into detail, or provide too many instructions.
    We can add more here as needed.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,6 +69,12 @@ module.exports = {
           'result-objects.md',
         ],
       },
+      {
+        title: 'Concepts',
+        children: [
+          'lifecycle.md',
+        ],
+      },
     ]
   },
 
