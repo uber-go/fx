a4dd747a068814d2feafe0c7e3b665cf4edab49c
Taiwon Chung <taiwon@uber.com>
2023-05-30 10:38:02 -0400

    doc(populate): add documentation on support for fx.Annotate with fx.Populate

--- populate.go
+++ populate.go
@@ -30,6 +30,34 @@ import (
 // values that must be populated. Pointers to structs that embed In are
 // supported, which can be used to populate multiple values in a struct.
 //
+// Annotating each pointer with ParamTags is also supported as a shorthand
+// to passing a pointer to a struct that embeds In with field tags. For example:
+//
+//	 var a A
+//	 var b B
+//	 fx.Populate(
+//		fx.Annotate(
+//				&a,
+//				fx.ParamTags(`name:"A"`)
+//	 	),
+//		fx.Annotate(
+//				&b,
+//				fx.ParamTags(`name:"B"`)
+//	 	)
+//	 )
+//
+// Code above is equivalent to the following:
+//
+//	type Target struct {
+//		fx.In
+//
+//		a A `name:"A"`
+//		b B `name:"B"`
+//	}
+//	var target Target
+//	...
+//	fx.Populate(&target)
+//
 // This is most helpful in unit tests: it lets tests leverage Fx's automatic
 // constructor wiring to build a few structs, but then extract those structs
 // for further testing.
@@ -49,12 +77,11 @@ func Populate(targets ...interface{}) Option {
 			rt  reflect.Type
 			tag reflect.StructTag
 		)
-		switch t.(type) {
+		switch t := t.(type) {
 		case annotated:
-			ann := t.(annotated)
-			rt = reflect.TypeOf(ann.Target)
-			tag = reflect.StructTag(ann.ParamTags[0])
-			targets[i] = ann.Target
+			rt = reflect.TypeOf(t.Target)
+			tag = reflect.StructTag(t.ParamTags[0])
+			targets[i] = t.Target
 		default:
 			rt = reflect.TypeOf(t)
 		}
