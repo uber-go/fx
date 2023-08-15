0e64bdbeb47a00dcfdec28951f8f20870cd5c6a9
Sung Yoon Whang <sungyoon@uber.com>
2023-08-15 11:59:45 -0700

    fix e2e

--- internal/e2e/go.mod
+++ internal/e2e/go.mod
@@ -1,8 +1,6 @@
 module go.uber.org/fx/internal/e2e
 
-go 1.21
-
-toolchain go1.21.0
+go 1.20
 
 require (
 	github.com/stretchr/testify v1.8.2
--- internal/fxreflect/stack_test_120.go
+++ internal/fxreflect/stack_test_120.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDeepStack(t *testing.T) {
