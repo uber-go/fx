491b6981f705888982f8a76c8a0f174fd25f7569
Abhinav Gupta <mail@abhinavg.net>
2024-02-07 04:59:31 -0800

    test: add a custom mock clock implementation
    benbjohnson/clock has been archived and no longer maintained.
    Removing that dependency is desirable.
    
    This change adds a mock clock adapted from Zap's mock clock
    (uber-go/zap#1349), but with operations specific to Fx's needs.
    
    There are two areas that require more scrutiny
    because of divergence from Zap's mock clock:
    
    WithTimeout requires us to implement a custom `context.Context`
    so that it can report context.DeadlineExceeded when it's time.
    We cannot just use `context.WithCancelCause` here because the cause
    for a context failure is considered different from `ctx.Err`,
    so `ctx.Err` would still report `context.Canceled` for a timeout.
    
    When testing that a sleep behaves as expected, there's a data race
    between the `Sleep` and the `Add` that progresses time.
    To resolve this, this change adds an `AwaitScheduled` method
    that blocks until there are operations scheduled for the future.
    This is done by using a `sync.Cond`.
    This gives us a way to wait until the sleep is scheduled
    before we progress time.
    
    This change also had to update Zap to pick up the release with the
    custom clock to drop the benbjohnson/clock dependency from the go.mod
    completely.
    
    Refs uber-go/zap#1349
    Resolves #1135

--- internal/fxclock/clock_test.go
+++ internal/fxclock/clock_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Uber Technologies, Inc.
+// Copyright (c) 2024 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,44 +22,150 @@ package fxclock
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
-	"github.com/benbjohnson/clock"
 	"github.com/stretchr/testify/assert"
 )
 
-var _ Clock = clock.Clock(nil)
-
-// Just a basic sanity check that everything is in order.
 func TestSystemClock(t *testing.T) {
-	t.Parallel()
-
 	clock := System
+	testClock(t, System, clock.Sleep)
+}
 
-	t.Run("Now and Since", func(t *testing.T) {
-		t.Parallel()
+func TestMockClock(t *testing.T) {
+	clock := NewMock()
+	testClock(t, clock, clock.Add)
+}
 
-		before := clock.Now()
-		assert.GreaterOrEqual(t, clock.Since(before), time.Duration(0))
+func testClock(t *testing.T, clock Clock, advance func(d time.Duration)) {
+	now := clock.Now()
+	assert.False(t, now.IsZero())
+
+	t.Run("Since", func(t *testing.T) {
+		advance(1 * time.Millisecond)
+		assert.NotZero(t, clock.Since(now), "time must have advanced")
 	})
 
 	t.Run("Sleep", func(t *testing.T) {
-		t.Parallel()
+		start := clock.Now()
+		go advance(1 * time.Millisecond)
+		clock.Sleep(1 * time.Millisecond)
 
-		assert.NotPanics(t, func() {
-			clock.Sleep(time.Millisecond)
-		})
+		assert.NotZero(t, clock.Since(start), "time must have advanced")
 	})
 
 	t.Run("WithTimeout", func(t *testing.T) {
-		t.Parallel()
-
-		ctx := context.Background()
-		ctx, cancel := clock.WithTimeout(ctx, time.Second)
+		ctx, cancel := clock.WithTimeout(context.Background(), 1*time.Millisecond)
 		defer cancel()
 
-		_, ok := ctx.Deadline()
-		assert.True(t, ok, "must have deadline")
+		t.Run("Deadline", func(t *testing.T) {
+			dl, ok := ctx.Deadline()
+			assert.True(t, ok, "must have a deadline")
+			assert.True(t, dl.After(now), "deadline must be in the future")
+		})
+
+		advance(1 * time.Millisecond)
+
+		select {
+		case <-ctx.Done():
+			assert.Error(t, ctx.Err(), "done context must error")
+			assert.ErrorIs(t, ctx.Err(), context.DeadlineExceeded,
+				"context must have exceeded its deadline")
+
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("expected context to be done")
+		}
 	})
+
+	t.Run("WithTimeout/Value", func(t *testing.T) {
+		type contextKey string
+		key := contextKey("foo")
+
+		ctx1 := context.WithValue(context.Background(), key, "bar")
+
+		ctx2, cancel := clock.WithTimeout(ctx1, 1*time.Millisecond)
+		defer cancel()
+
+		assert.Equal(t, "bar", ctx2.Value(key), "value must be preserved")
+	})
+
+	t.Run("WithTimeout/Cancel", func(t *testing.T) {
+		ctx, cancel := clock.WithTimeout(context.Background(), 1*time.Millisecond)
+		cancel()
+
+		select {
+		case <-ctx.Done():
+			assert.Error(t, ctx.Err(), "done context must error")
+			assert.ErrorIs(t, ctx.Err(), context.Canceled,
+				"context must have been canceled")
+
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("expected context to be done")
+		}
+	})
+}
+
+func TestMock_Sleep(t *testing.T) {
+	clock := NewMock()
+
+	ch := make(chan struct{})
+	go func() {
+		clock.Sleep(2 * time.Millisecond)
+		close(ch)
+	}()
+
+	// We cannot advance time until we're certain
+	// that the Sleep call has started waiting.
+	// Otherwise, we'll advance that one millisecond,
+	// and then the Sleep will start waiting for another Advance,
+	// which will never come.
+	//
+	// AwaitScheduled will block until there is at least one
+	// scheduled event.
+	clock.AwaitScheduled(1)
+
+	// Adbance only one millisecond, the Sleep should not return.
+	clock.Add(1 * time.Millisecond)
+	select {
+	case <-ch:
+		t.Fatal("sleep should not have returned")
+	case <-time.After(1 * time.Millisecond):
+		// ok
+	}
+
+	// Avance to the next millisecond, the Sleep should return.
+	clock.Add(1 * time.Millisecond)
+	select {
+	case <-ch:
+		// ok
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("expected Sleep to return")
+	}
+}
+
+func TestMock_AddNegative(t *testing.T) {
+	clock := NewMock()
+	assert.Panics(t, func() { clock.Add(-1) })
+}
+
+func TestMock_ManySleepers(t *testing.T) {
+	const N = 100
+
+	clock := NewMock()
+
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+
+			clock.Sleep(1 * time.Millisecond)
+		}()
+	}
+
+	clock.AwaitScheduled(N)
+	clock.Add(1 * time.Millisecond)
+	wg.Wait()
 }
